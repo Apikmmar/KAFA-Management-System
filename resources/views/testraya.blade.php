5a48ee4d72930198f325f26f4d9d9b32426821d7
apikammar <apikammar07@gmail.com>
2024-04-10 18:34:13 +0800

    create master, css for all module

---
+++ resources/views/testraya.blade.php
@@ -0,0 +1,10 @@
+@extends('component.master')
+
+@section('content')
+    <div class="container">
+        <div class="text-center">
+            <p class="h2">SELAMAT HARI RAYA AIDIL FITRI MAAF ZAHIR DAN BATIN</p>
+            <img src="{{ asset('default_image/mantantersakiti.jpeg') }}" class="rounded" alt="matriye.jpeg">
+        </div>
+    </div>
+@endsection
